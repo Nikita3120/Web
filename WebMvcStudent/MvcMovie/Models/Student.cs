3e71a460b3e2a084a27a20714d831b2f9cf013c3
Your Name <you@example.com>
2020-11-12 10:39:25 +0300

    v1

---
+++ WebMvcStudent/MvcMovie/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+
+namespace MvcMovie.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public decimal Rating { get; set; }
+    }
+
+    public class StudentDBContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+    }
+}
